214f8763972680d5c1be8c4a3730a53e837818bd
Reza Hidayat <97022190+rezapace@users.noreply.github.com>
2022-11-15 23:31:13 +0700

    Add files via upload

---
+++ reza_50420900_pert8/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Hello, I Muhammad Reza Hidayat")
+}
+
+// Windows PowerShell
+// Copyright (C) Microsoft Corporation. All rights reserved.
+
+// Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+// PS C:\Users\R\Desktop\reza_50420900_pert8> go run hello.go
+// Hello, World!
+// Hello, I Muhammad Reza Hidayat
+// PS C:\Users\R\Desktop\reza_50420900_pert8>
+
+// Windows PowerShell
+// Copyright (C) Microsoft Corporation. All rights reserved.
+
+// Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+// PS C:\Users\R\Desktop\reza_50420900_pert8> go build hello.go
+// PS C:\Users\R\Desktop\reza_50420900_pert8>
